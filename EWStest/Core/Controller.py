8130371be3dc07b80d17e5d143822da6a8cf9304
211_0@naver.com <211_0@naver.com>
2023-11-17 22:42:56 +0900

    테스트

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -206,6 +206,7 @@ class Controller:
                     # print("find_flag == True: ", find_flag == True)  # 테스트용
                     # print("x_dir == len(right_left): ", x_dir == len(right_left))  # 테스트용
                     self.robo._motion.x_head_angle = -(right_left[x_dir-1])
+                    print("self.robo._motion.x_head_angle: ", self.robo._motion.x_head_angle)
                     break
             self.robo._motion.set_head("LEFTRIGHT_CENTER") # 고개 원위치로 (가운데로)
             time.sleep(0.2)
