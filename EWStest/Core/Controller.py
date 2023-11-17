43937d99d45324866037946515b1d55fbc17743a
211_0@naver.com <211_0@naver.com>
2023-11-17 22:31:56 +0900

    테스트 전 코드로 돌아가기

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -186,7 +186,6 @@ class Controller:
                 if (find_flag == True) or (x_dir == len(right_left)):
                     # print("find_flag == True: ", find_flag == True)  # 테스트용
                     # print("x_dir == len(right_left): ", x_dir == len(right_left))  # 테스트용
-                    self.robo._motion.x_head_angle = right_left[x_dir-1]
                     break
             self.robo._motion.set_head("LEFTRIGHT_CENTER") # 고개 원위치로 (가운데로)
             time.sleep(0.2)
@@ -205,7 +204,6 @@ class Controller:
                 if (find_flag == True) or (x_dir == len(right_left)):
                     # print("find_flag == True: ", find_flag == True)  # 테스트용
                     # print("x_dir == len(right_left): ", x_dir == len(right_left))  # 테스트용
-                    self.robo._motion.x_head_angle = -right_left[x_dir-1]
                     break
             self.robo._motion.set_head("LEFTRIGHT_CENTER") # 고개 원위치로 (가운데로)
             time.sleep(0.2)
@@ -221,13 +219,6 @@ class Controller:
         flagycenter = FlagyCenterMeasurer(img_width=640, img_height=480)
 
         correctAngle = 0  # 깃발이 센터에 왔을 때 1로 변경
-
-        if self.robo._motion.x_head_angle < 0:
-            self.robo._motion.set_head("LEFT", -self.robo._motion.x_head_angle)
-        elif self.robo._motion.x_head_angle > 0:
-            self.robo._motion.set_head("RIGHT", self.robo._motion.x_head_angle)
-        else:
-            print("로봇 머리 각도는 정면입니다.")
 
         # 깃발을 못 찾았을 때 반환하는 값
 
