a6bbf789dee362f1a0563003101eabdf7873a3f4
211_0@naver.com <211_0@naver.com>
2023-11-17 22:03:20 +0900

    테스트

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -187,8 +187,9 @@ class Controller:
                     # print("find_flag == True: ", find_flag == True)  # 테스트용
                     # print("x_dir == len(right_left): ", x_dir == len(right_left))  # 테스트용
                     break
-                self.robo._motion.set_head("LEFTRIGHT_CENTER") # 고개 원위치로 (가운데로)
-                time.sleep(0.2)
+                else:
+                    self.robo._motion.set_head("LEFTRIGHT_CENTER") # 고개 원위치로 (가운데로)
+                    time.sleep(0.2)
             
             x_dir = 0
             # 고개 왼쪽으로 찾기
@@ -205,8 +206,8 @@ class Controller:
                     # print("find_flag == True: ", find_flag == True)  # 테스트용
                     # print("x_dir == len(right_left): ", x_dir == len(right_left))  # 테스트용
                     break
-                self.robo._motion.set_head("LEFTRIGHT_CENTER")
-                time.sleep(0.2)
+            # self.robo._motion.set_head("LEFTRIGHT_CENTER")
+            # time.sleep(0.2)
 
             # 여기까지 오면 깃발을 찾은 상황 -> 깃발 센터 맞추는 함수로 넘어가기
             # self.check_flag_distance()
