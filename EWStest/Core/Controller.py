bac79dd15c68f29546a3ac6c5489caf08778fae5
HyunWoo Cho <hwooo22@kookmin.ac.kr>
2023-11-19 19:43:03 +0900

    TeeShot에서 Center에 있을 때 움직이는 모션

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -884,17 +884,19 @@ class Controller:
 
             elif self.C_left == 1:
                 print("로봇: 가운데, 공: 왼쪽")
+                
                 self.robo._motion.set_head("LEFTRIGHT_CENTER")
                 time.sleep(0.5)
-                self.robo._motion.walk_side("LEFT", 4)
+                self.robo._motion.walk_side("LEFT", 10)
 
                 # 이 밑 부분은 확인을 통해서 바꿔야 함.
                 self.robo._motion.turn("RIGHT", 20, 2)
                 time.sleep(0.8)
-                self.robo._motion.walk_side("LEFT", 2)
+                self.robo._motion.walk_side("LEFT", 3)
                 time.sleep(0.5)
-                self.robo._motion.turn("RIGHT", 20, 3)
+                self.robo._motion.turn("RIGHT", 20)
                 time.sleep(0.8)
+                self.robo._motion.walk_side("RIGHT")
 
                 self.ball_feature_ball()
                 time.sleep(1)
