c026a11be9f72e25d3ae6f4c115aae3906c83861
chloebh9 <chloebh9@naver.com>
2023-11-21 18:23:16 +0900

    중간 작업..

--- EWStest/test_file/new_GoalDetection.py
+++ EWStest/test_file/new_GoalDetection.py
@@ -2,7 +2,26 @@ import numpy as np
 import cv2
 
 class NewGoalDetection:
-    def __init__(self, video_path):
+    def __init__(self, video_path, img_width=640, img_height=480, width=4, focal=450):
+        self.dist = 0 
+        self.focal = focal
+        self.pixels = 30
+        self.width = width
+
+        self.img_width = img_width
+        self.img_height = img_height
+        self.img_width_middle = img_width // 2
+        self.img_height_middle = img_height // 2
+
+        self.kernel = np.ones((3, 3), 'uint8')
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.org = (0, 20)
+        self.fontScale = 0.6
+        self.color = (0, 0, 255)
+        self.thickness = 2
+        
+        self.middle = img_width // 2
+        
         self.cap = cv2.VideoCapture(video_path, cv2.CAP_V4L)
         if not self.cap.isOpened():
             raise ValueError(f"Video at {video_path} cannot be opened")
@@ -64,6 +83,8 @@ class NewGoalDetection:
                 break
 
             hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            
+            kernel = np.ones((3,3),'uint8')
 
             # 녹색 범위 정의
             low_green = np.array([57, 78, 61])
@@ -73,8 +94,10 @@ class NewGoalDetection:
             self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
 
             # 노랑색 범위 정의
-            low_yellow = np.array([0, 16, 144])
-            high_yellow = np.array([43, 184, 255])
+            # low_yellow = np.array([0, 16, 144])
+            # high_yellow = np.array([43, 184, 255])
+            low_yellow = np.array([20, 90, 144])
+            high_yellow = np.array([45, 200, 255])
             yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
             for green_box in self.green_boxes:
@@ -103,10 +126,52 @@ class NewGoalDetection:
                             # flag_centers 리스트에 중점값 추가
                             flag_centers.append((cx, cy))
 
+                # 공 찾기 추가
+                # ball hsv
+                lower1 = np.array([0, 100, 50])
+                upper1 = np.array([10, 200, 200])
+                lower = np.array([137, 0, 0])
+                upper = np.array([200, 255, 255])
+                mask = cv2.inRange(hsv_frame, lower, upper)
+                mask += cv2.inRange(hsv_frame, lower1, upper1)
+
+
+                #Remove Extra garbage from image
+                d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations = 5)
+
+
+                #find the histogram -> 공
+                cont,hei = cv2.findContours(d_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+                cont = sorted(cont, key = cv2.contourArea, reverse = True)[:1]
+                
+                b_max_x, b_min_x = 0, 0
+                b_max_y, b_min_y = 0, 0
+                
+                is_goal = False
+            
+                if len(cont) > 0:
+                    ball_cnt = cont[0]
+                    #check for contour area
+                    if (cv2.contourArea(ball_cnt)>70 and cv2.contourArea(ball_cnt)<306000):
+                        
+                        #Draw a rectange on the contour
+                        rect = cv2.minAreaRect(ball_cnt)
+                        box = cv2.boxPoints(rect)
+                        box = np.int0(box)
+                        print('ball points :', box)
+                        cv2.drawContours(img, [box], -1, (255,0,0), 3)
+
+                        b_max_x, b_min_x = self.getMaxMin(box)
+                        b_max_y, b_min_y = self.getyMaxMin(box)
+                        isMiddle = self.judgeMiddle(b_max_x, b_min_x)
+                        
+                        img = self.get_dist(rect, img, 'ball', isMiddle)
+                
                 # flag_centers가 비어있지 않을 때만 실행
                 if flag_centers:
                     # flag_centers 리스트에서 중점값이 가장 높은 flag 선택
                     farthest_flag_center = min(flag_centers, key=lambda center: center[1])
+                    # farthest_flag_center[0] -> x 중점 좌표, farthest_flag_center[1] -> y 중점 좌표
                     # 해당 flag의 박스 그리기
                     cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
                                   (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
@@ -114,6 +179,22 @@ class NewGoalDetection:
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                     # farthest_flag_boxes 리스트에 중점값과 "FLAG" 추가
                     self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
+                    
+                    goal_range = 22
+                    # 공이 (홀컵기준)밑에 있을 때
+                    if (f_min_y + f_max_y)/2 < (b_min_y + b_max_y)/2:
+                        if f_min_x + goal_range <= b_min_x and b_max_x <= f_max_x - goal_range and f_min_y <= b_min_y and b_max_y <= f_max_y - goal_range:
+                            print("Goal!")
+                            is_goal = True
+                            cv2.putText(img, 'Goal!', (self.img_width_middle - 200, self.img_height_middle - 200), self.font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                            # return is_goal
+                    # 공이 (홀컵기준)위에 있을 때
+                    else:
+                        if f_min_x + goal_range <= b_min_x and b_max_x <= f_max_x - goal_range and f_min_y - goal_range <= b_min_y and b_max_y <= f_max_y - goal_range:
+                            print("Goal!")
+                            is_goal = True
+                            cv2.putText(img, 'Goal!', (self.img_width_middle - 200, self.img_height_middle - 200), self.font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                            # return is_goal
 
             # Display the original frame
             cv2.imshow('Frame', frame)
@@ -129,6 +210,7 @@ class NewGoalDetection:
         self.cap.release()
         cv2.destroyAllWindows()
         return farthest_flag_center
+    
 if __name__ == "__main__":
     video_path = 0  # Use 0 for webcam
     new_goaldetector = NewGoalDetection(video_path)
