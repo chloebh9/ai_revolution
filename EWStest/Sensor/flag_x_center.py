6c96c859f52107601376c50e9a58a31a39a243f7
daeheejang <eowkdeogml9409@kookmin.ac.kr>
2023-11-21 17:04:21 +0900

    add imShow

--- EWStest/Sensor/flag_x_center.py
+++ EWStest/Sensor/flag_x_center.py
@@ -103,13 +103,13 @@ class FlagxCenterMeasurer:
                     have_flag = True
                     
             break
-        #     cv2.imshow('프레임', frame)
-        #     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #         break
-        # cv2.destroyAllWindows()
+            cv2.imshow('프레임', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
 
-        # if self.farthest_flag_boxes:
-        #     max_x, min_x, max_y, min_y = self.max_x, self.min_x, self.max_y, self.min_y
+        if self.farthest_flag_boxes:
+            max_x, min_x, max_y, min_y = self.max_x, self.min_x, self.max_y, self.min_y
 
         flag_x_isMiddle = self.judgeMiddle(max_x, min_x)
         return [flag_x_isMiddle, farthest_flag_center[0], farthest_flag_center[1], have_flag]
