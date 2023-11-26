4558f47be1d0a4dc59580f6baad45f8210063d6a
211_0@naver.com <211_0@naver.com>
2023-11-26 19:51:01 +0900

    테스트

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -927,7 +927,7 @@ class Controller:
             ballycenter = BallyCenterMeasurer(img_width=640, img_height=480)
             ball_y_angle = ["N"]  # 공을 못 찾았을 때 반환하는 값
             correctAngle = 0
-            putting_angle = 30
+            putting_angle = 20
             putting_angle_error = 2
             dist_Process = DistMeasurer()
 
