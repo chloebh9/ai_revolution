971df20d5008749c6b95410b71061e675a1be66c
woojin <owj1207@gmail.com>
2023-11-28 15:53:00 +0900

    ball_x_center.py 모폴로지 연산 제거

--- EWStest/Sensor/ball_x_center.py
+++ EWStest/Sensor/ball_x_center.py
@@ -110,7 +110,7 @@ class BallxCenterMeasurer:
             mask += cv2.inRange(hsv_img, lower1, upper1)
 
             #모폴로지 연산
-            d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel, iterations = 5)
+            # d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel, iterations = 5)
             
             cont,hei = cv2.findContours(d_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
             cont = sorted(cont, key = cv2.contourArea, reverse = True)[:1]
