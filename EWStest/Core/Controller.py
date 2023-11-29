3e01f0ccf7c0b0c5e2e3a97d3d91c941f8c8be64
woojin <owj1207@gmail.com>
2023-11-29 19:26:32 +0900

    퍼팅하는 부분에서 고개 센터로 맞추는 코드 넣음

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -1165,8 +1165,9 @@ class Controller:
             # dist_Process = DistMeasurer()
             # self.check_ball_distance()
             # self.putting_robot_turn()
-
+            self.robo._motion.set_head("LEFTRIGHT_CENTER")
             while correctAngle != 1:
+
                 # 퍼팅 위치까지 가고, 공 앞에서 돌아야할 각도만큼 돌았는데 공이 없을시, 공을 찾고 몸을 공과 일자로 맞추는 코드
                 self.ball_feature_ball()
                 
