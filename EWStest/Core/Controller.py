840e71a3ff0426254846533ae047619199d631cf
HyunWoo Cho <hwooo2255@gmail.com>
2023-12-01 01:22:52 +0900

    test

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -891,7 +891,7 @@ class Controller:
 
             elif self.C_center == 1:
                 print("로봇: 가운데, 공: 가운데")
-                self.robo._motion.walk_side("LEFT", 4) # 공을 발로 차는걸 예방하기 위해서 왼쪽으로 먼저 이동.
+                self.robo._motion.walk_side("LEFT", 6) # 공을 발로 차는걸 예방하기 위해서 왼쪽으로 먼저 이동.
                 time.sleep(0.5)
                 self.robo._motion.turn("RIGHT", 20, 4) # 90도 회전
                 time.sleep(0.8)
