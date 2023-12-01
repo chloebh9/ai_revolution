b1fefa3789f471b7c8cc09f99ca22d8251867975
daeheejang <eowkdeogml9409@kookmin.ac.kr>
2023-12-01 22:03:09 +0900

    hsv

--- EWStest/Sensor/ball_y_center.py
+++ EWStest/Sensor/ball_y_center.py
@@ -86,12 +86,12 @@ class BallyCenterMeasurer:
             img = cv2.dilate(img, self.kernel, iterations=1)
             hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-            lower1 = np.array([10, 43, 80])
-            upper1 = np.array([28, 119, 230])
+            lower1 = np.array([0, 0, 43])
+            upper1 = np.array([19, 183, 200])
             # lower1 = np.array([0, 100, 50])
             # upper1 = np.array([10, 200, 200])
-            lower = np.array([137, 0, 0])
-            upper = np.array([200, 255, 255])
+            lower = np.array([167,135, 119])
+            upper = np.array([187, 255, 255])
             mask = cv2.inRange(hsv_img, lower, upper)
             mask += cv2.inRange(hsv_img, lower1, upper1)
 
