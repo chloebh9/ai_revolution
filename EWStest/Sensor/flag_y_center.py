46d3cc8657682bd8770004e3c043ccae5378e8fb
woojin <owj1207@gmail.com>
2023-12-02 21:24:56 +0900

    test

--- EWStest/Sensor/flag_y_center.py
+++ EWStest/Sensor/flag_y_center.py
@@ -151,12 +151,12 @@ class FlagyCenterMeasurer:
                 else:
                     cv2.putText(frame, f'Shape: {shape_text}', (center[0], center[1] + offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-            is_y_middle = self.judgeMiddle(farthest_center, self.img_height)
+            # is_y_middle = self.judgeMiddle(farthest_center, self.img_height)
 
             
             # return is_y_middle
             cv2.imshow('Green and Yellow Frame', frame)
-            print(is_y_middle)
+            # print(is_y_middle)
 
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
@@ -165,7 +165,6 @@ class FlagyCenterMeasurer:
 
         cap.release()
         cv2.destroyAllWindows()
-        return is_y_middle
 
 if __name__ == "__main__":
     shape_recognition = FlagyCenterMeasurer(img_width=640, img_height=480)
