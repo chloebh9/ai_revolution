8e567e10babb504bde867e5914ee7ba2112dace1
daeheejang <eowkdeogml9409@kookmin.ac.kr>
2023-12-03 15:23:07 +0900

    오류수정

--- EWStest/test_file/GD.py
+++ EWStest/test_file/GD.py
@@ -1,5 +1,3 @@
-# from Sensor.HSVAdjust import MaskGenerator
-
 import numpy as np
 import cv2
 
@@ -14,8 +12,6 @@ class FlagxCenterMeasurer:
         self.min_y = None
         self.farthest_flag_boxes = []
 
-    # 나머지 코드는 여기에 있어야 합니다
-    # .
     def getMaxMin(self, box):
         min_x, max_x = self.img_width, 0
         min_y, max_y = self.img_height, 0
@@ -28,16 +24,10 @@ class FlagxCenterMeasurer:
 
         return max_x, min_x, max_y, min_y
 
-    def check_goal(self, frame, green_box, red_boxes):
-        x, y, w, h = green_box
-        for red_box in red_boxes:
-            rx, ry, rw, rh = red_box
-            ball_center_x = rx + rw // 2
-            ball_center_y = ry + rh // 2
-            if x < ball_center_x < x + w and y < ball_center_y < y + h:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, 'Goal', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                break  # 공이 한 번만 골로 인식되도록 하려면 이 줄을 제거하세요.
+    def check_goal(self, frame, ball_center, flag_boxes):
+        for (flag_x, flag_y, _) in flag_boxes:
+            if flag_x < ball_center[0] < flag_x + self.img_width / 10 and flag_y < ball_center[1] < flag_y + self.img_height / 10:
+                cv2.putText(frame, 'Goal', (int(ball_center[0]), int(ball_center[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     def run(self):
         cap = cv2.VideoCapture(0, cv2.CAP_V4L)
@@ -106,13 +96,22 @@ class FlagxCenterMeasurer:
             # 공에 대한 마스크 생성
             red_mask = cv2.inRange(hsv_frame, red_lower1, red_upper1) + cv2.inRange(hsv_frame, red_lower2, red_upper2)
 
-            # 공의 경계를 찾아서 공 박스를 가져옵니다.
-            contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            red_boxes = [cv2.boundingRect(contour) for contour in contours]
+            # 공의 중심 좌표 계산
+            ball_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            ball_centers = []
 
-            # 플래그 박스와 공 박스를 비교하여 공이 플래그 내부에 있는지 확인
-            for green_box in self.green_boxes:
-                self.check_goal(frame, green_box, red_boxes)
+            for cnt in ball_contours:
+                area = cv2.contourArea(cnt)
+                if area > 10:
+                    M = cv2.moments(cnt)
+                    if M['m00'] != 0:
+                        cx = int(M['m10'] / M['m00'])
+                        cy = int(M['m01'] / M['m00'])
+                        ball_centers.append((cx, cy))
+
+            # 플래그 박스와 공의 중심 좌표를 비교하여 골을 확인
+            if have_flag:
+                self.check_goal(frame, farthest_flag_center, ball_centers)
 
             cv2.imshow('프레임', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -126,7 +125,6 @@ class FlagxCenterMeasurer:
             flag_x_isMiddle = "N"
 
         return [flag_x_isMiddle, farthest_flag_center[0], farthest_flag_center[1], have_flag]
-
 
 if __name__ == "__main__":
     video_path = 0  # 웹캠을 사용하려면 0을 사용
