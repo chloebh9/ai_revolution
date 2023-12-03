49dbac846db3bbf8e629a02114497801371e8590
daeheejang <eowkdeogml9409@kookmin.ac.kr>
2023-12-03 13:23:08 +0900

    farthest flag 조정

--- EWStest/test_file/GD.py
+++ EWStest/test_file/GD.py
@@ -18,27 +18,27 @@ class ShapeRecognition:
 
             hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             # 빨간색 범위 정의
-            lower1 = np.array([0, 0, 132])
-            upper1 = np.array([19, 183, 200])
-            lower2 = np.array([165, 134, 118])
-            upper2 = np.array([187, 255, 255])
 
-            # 빨간색 마스크 생성
-            red_mask1 = cv2.inRange(hsv_frame, lower1, upper1)
-            red_mask2 = cv2.inRange(hsv_frame, lower2, upper2)
-            red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+            upper1 = np.array([19, 183, 200])
+            # lower1 = np.array([0, 100, 50])
+            # upper1 = np.array([10, 200, 200])
+            lower = np.array([167,135, 119])
+            upper = np.array([187, 255, 255])
+            mask2 = cv2.inRange(hsv_img, lower, upper)
+            mask1= cv2.inRange(hsv_img, lower1, upper1)
+            red_mask = cv2.bitwise_or(mask1, mask2)
 
             # 녹색 범위 정의
-            low_green = np.array([57, 78, 61])
-            high_green = np.array([89, 255, 255])
+            low_green = np.array([35, 84, 0])
+            high_green = np.array([255, 255, 141])
             green_mask = cv2.inRange(hsv_frame, low_green, high_green)
-            result_frame = cv2.bitwise_and(frame, frame, mask=green_mask)
+
             contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
 
             # 노랑색 범위 정의
-            low_yellow = np.array([0, 16, 144])
-            high_yellow = np.array([43, 184, 255])
+            low_yellow = np.array([21, 56, 171])
+            high_yellow = np.array([97, 255, 255])
             yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
             for green_box in self.green_boxes:
