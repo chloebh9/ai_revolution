a470ddfced1611eee2306317bd0f8839b9033e28
HyunWoo Cho <hwooo2255@gmail.com>
2023-12-03 18:38:57 +0900

    test

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -1348,25 +1348,25 @@ class Controller:
             print("퍼팅하겠습니다")
             print("퍼팅할 당시의 거리: ", flag_ball_dis)
 
-            if shot_way == "R":
-                self.robo._motion.set_head("LEFT", 90)
+            # if shot_way == "R":
+            #     self.robo._motion.set_head("LEFT", 90)
 
-            else:
-                self.robo._motion.set_head("RIGHT", 90)
+            # else:
+            #     self.robo._motion.set_head("RIGHT", 90)
 
-            flagxcenter = FlagxCenterMeasurer(img_width=640, img_height=480)
+            # flagxcenter = FlagxCenterMeasurer(img_width=640, img_height=480)
 
-            for i in range(3):
-                flag_x_angle = flagxcenter.run()
-                print(flag_x_angle)
-                if flag_x_angle[0] == "R":
-                    self.robo._motion.turn("RIGHT", 5)
+            # for i in range(3):
+            #     flag_x_angle = flagxcenter.run()
+            #     print(flag_x_angle)
+            #     if flag_x_angle[0] == "R":
+            #         self.robo._motion.turn("RIGHT", 5)
                     
-                if flag_x_angle[0] == "L":
-                    self.robo._motion.turn("LEFT", 5)
-                if flag_x_angle[0] == "C":
-                    print("중앙")
-                    break
+            #     if flag_x_angle[0] == "L":
+            #         self.robo._motion.turn("LEFT", 5)
+            #     if flag_x_angle[0] == "C":
+            #         print("중앙")
+            #         break
 
             self.robo._motion.hit_the_ball("LEFT", dist=flag_ball_dis)
             time.sleep(6)
