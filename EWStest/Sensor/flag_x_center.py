8c5386c9168cc9b871e247659d73ff5f0302e1af
woojin <owj1207@gmail.com>
2023-12-03 17:57:18 +0900

    ddddd

--- EWStest/Sensor/flag_x_center.py
+++ EWStest/Sensor/flag_x_center.py
@@ -50,78 +50,80 @@ class FlagxCenterMeasurer:
 
     def run(self):
         cap = cv2.VideoCapture(0, cv2.CAP_V4L)
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("프레임 캡처에 실패했습니다.")
-                break
-                
-            have_flag = False # 화면에 flag가 있으면 True, 없으면 False
-            farthest_flag_center = [0, 0] # 화면에 flag가 있으면 True, 없으면 False
-            
-            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            low_green = np.array([35, 84, 0])
-            high_green = np.array([255, 255, 141])
-            green_mask = cv2.inRange(hsv_frame, low_green, high_green)
-
-            contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
-
-            # yellow_mask = MaskGenerator.flag_generate_mask(hsv_frame)
-            low_yellow = np.array([21, 56, 171])
-            high_yellow = np.array([97, 255, 255])
-            yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-
-            max_x, min_x = 0,0 # 깃발을 못 찾았을 때 오류나는 것을 방지하기 위해 바운딩 박스의 좌표를 0으로 초기화
-
-            for green_box in self.green_boxes:
-                x, y, w, h = green_box
-                green_roi = frame[y:y+h, x:x+w]
-                yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
-                yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                flag_centers = []
-
-                for cnt in yellow_contours:
-                    area = cv2.contourArea(cnt)
-                    if area > 10:
-                        rect = cv2.minAreaRect(cnt)
-                        box = cv2.boxPoints(rect)
-                        box = np.int0(box)
-                        max_x, min_x, max_y, min_y = self.getMaxMin(box)
-                        cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
-                        M = cv2.moments(cnt)
-                        if M['m00'] != 0:
-                            cx = int(M['m10'] / M['m00'])
-                            cy = int(M['m01'] / M['m00'])
-                            cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                            flag_centers.append((cx, cy))
-
-                if flag_centers:
-                    farthest_flag_center = min(flag_centers, key=lambda center: center[1])
-                    cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
-                                  (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
-                    cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
-                    have_flag = True
+        for i in range(30):
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    print("프레임 캡처에 실패했습니다.")
+                    break
                     
-            break
-        #     cv2.imshow('프레임', frame)
-        #     cv2.imshow("mask ", yellow_mask)
-        #     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #         break
-        # cv2.destroyAllWindows()
+                have_flag = False # 화면에 flag가 있으면 True, 없으면 False
+                farthest_flag_center = [0, 0] # 화면에 flag가 있으면 True, 없으면 False
+                
+                hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # if self.farthest_flag_boxes:
-        #     max_x, min_x, max_y, min_y = x+farthest_flag_center[0], x, y+farthest_flag_center[0], y
-            # max_x, min_x, max_y, min_y = self.max_x, self.min_x, self.max_y, self.min_y
+                low_green = np.array([35, 84, 0])
+                high_green = np.array([255, 255, 141])
+                green_mask = cv2.inRange(hsv_frame, low_green, high_green)
 
-        if have_flag == True:
-            flag_x_isMiddle = self.judgeMiddle(max_x, min_x)
-        else:
-            flag_x_isMiddle = "N"
+                contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
+
+                # yellow_mask = MaskGenerator.flag_generate_mask(hsv_frame)
+                low_yellow = np.array([21, 56, 171])
+                high_yellow = np.array([97, 255, 255])
+                yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
+
+                max_x, min_x = 0,0 # 깃발을 못 찾았을 때 오류나는 것을 방지하기 위해 바운딩 박스의 좌표를 0으로 초기화
+
+                for green_box in self.green_boxes:
+                    x, y, w, h = green_box
+                    green_roi = frame[y:y+h, x:x+w]
+                    yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
+                    yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                    flag_centers = []
+
+                    for cnt in yellow_contours:
+                        area = cv2.contourArea(cnt)
+                        if area > 10:
+                            rect = cv2.minAreaRect(cnt)
+                            box = cv2.boxPoints(rect)
+                            box = np.int0(box)
+                            max_x, min_x, max_y, min_y = self.getMaxMin(box)
+                            cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
+                            M = cv2.moments(cnt)
+                            if M['m00'] != 0:
+                                cx = int(M['m10'] / M['m00'])
+                                cy = int(M['m01'] / M['m00'])
+                                cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                                flag_centers.append((cx, cy))
+
+                    if flag_centers:
+                        farthest_flag_center = min(flag_centers, key=lambda center: center[1])
+                        cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
+                                    (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
+                        cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                        self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
+                        have_flag = True
+                        
+                break
+            #     cv2.imshow('프레임', frame)
+            #     cv2.imshow("mask ", yellow_mask)
+            #     if cv2.waitKey(1) & 0xFF == ord('q'):
+            #         break
+            # cv2.destroyAllWindows()
+
+            # if self.farthest_flag_boxes:
+            #     max_x, min_x, max_y, min_y = x+farthest_flag_center[0], x, y+farthest_flag_center[0], y
+                # max_x, min_x, max_y, min_y = self.max_x, self.min_x, self.max_y, self.min_y
+
+            if have_flag == True:
+                flag_x_isMiddle = self.judgeMiddle(max_x, min_x)
+            else:
+                flag_x_isMiddle = "N"
+            print(flag_x_isMiddle)
             
         return [flag_x_isMiddle, farthest_flag_center[0], farthest_flag_center[1], have_flag]
 
