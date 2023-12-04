1d7ce372dcd40ca14737a795a092bce63bd5b7d4
HyunWoo Cho <hwooo2255@gmail.com>
2023-12-05 01:57:12 +0900

    test

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -1145,8 +1145,7 @@ class Controller:
             self.robo._motion.hit_the_ball("LEFT")
             # +================================== 여기까지 추가 ================================================+
             time.sleep(0.1)
-            self.robo._motion.turn("LEFT", 45, 3, 0.5)   # 티샷 끝나고 깃발 찾기 위해 턴
-            self.robo._motion.turn("LEFT",20)
+            self.robo._motion.turn("LEFT", 45, 2, 6)   # 티샷 끝나고 깃발 찾기 위해 턴
             print("왼쪽으로 90도 회전")
 
             self.act = act.SEARCH_FLAG
