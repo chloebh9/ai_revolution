c44bd4134ce79c2a7ace9d06b0960bffdbe00e61
woojin <owj1207@gmail.com>
2023-12-06 23:59:34 +0900

    test

--- EWStest/Sensor/flag_x_center.py
+++ EWStest/Sensor/flag_x_center.py
@@ -56,80 +56,79 @@ class FlagxCenterMeasurer:
         cap.set(3, W_View_size)
         cap.set(4, H_View_size)
         cap.set(5, FPS)
-        for i in range(10):
-            while True:
-                ret, frame = cap.read()
-                if not ret:
-                    print("프레임 캡처에 실패했습니다.")
-                    break
-                    
-                have_flag = False # 화면에 flag가 있으면 True, 없으면 False
-                farthest_flag_center = [0, 0] # 화면에 flag가 있으면 True, 없으면 False
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("프레임 캡처에 실패했습니다.")
+                break
                 
-                hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            have_flag = False # 화면에 flag가 있으면 True, 없으면 False
+            farthest_flag_center = [0, 0] # 화면에 flag가 있으면 True, 없으면 False
+            
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-                low_green = np.array([35, 110, 114])
-                high_green = np.array([255, 255, 255])
-                green_mask = cv2.inRange(hsv_frame, low_green, high_green)
-                
-                # 모폴로지 연산 추가
-                k = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-                cv2.morphologyEx(hsv_frame, cv2.MORPH_CLOSE, k)
+            low_green = np.array([35, 110, 114])
+            high_green = np.array([255, 255, 255])
+            green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+            
+            # 모폴로지 연산 추가
+            k = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+            cv2.morphologyEx(hsv_frame, cv2.MORPH_CLOSE, k)
 
-                contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
+            contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
 
-                # yellow_mask = MaskGenerator.flag_generate_mask(hsv_frame)
-                # low_yellow = np.array([21, 56, 171])
-                # # high_yellow = np.array([97, 255, 255])
-                # low_yellow = np.array([0,105,151])
-                # high_yellow = np.array([43,255,255])
-                # yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-                # lower9 = np.array( [26 , 52 , 151] )
-                # upper9 = np.array( [50 , 100 , 255] )
-                # yellow_mask += cv2.inRange(hsv_frame, lower9 , upper9 )
-                low_yellow = np.array([23, 82, 121])
-                high_yellow = np.array([40, 200, 230])
-                yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-                
-                # #대회장 version
-                # low_yellow = np.array([23, 81, 121])
-                # high_yellow = np.array([43, 223, 255])
-                # yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
+            # yellow_mask = MaskGenerator.flag_generate_mask(hsv_frame)
+            # low_yellow = np.array([21, 56, 171])
+            # # high_yellow = np.array([97, 255, 255])
+            # low_yellow = np.array([0,105,151])
+            # high_yellow = np.array([43,255,255])
+            # yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
+            # lower9 = np.array( [26 , 52 , 151] )
+            # upper9 = np.array( [50 , 100 , 255] )
+            # yellow_mask += cv2.inRange(hsv_frame, lower9 , upper9 )
+            low_yellow = np.array([23, 82, 121])
+            high_yellow = np.array([40, 200, 230])
+            yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
+            
+            # #대회장 version
+            # low_yellow = np.array([23, 81, 121])
+            # high_yellow = np.array([43, 223, 255])
+            # yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
-                max_x, min_x = 0,0 # 깃발을 못 찾았을 때 오류나는 것을 방지하기 위해 바운딩 박스의 좌표를 0으로 초기화
+            max_x, min_x = 0,0 # 깃발을 못 찾았을 때 오류나는 것을 방지하기 위해 바운딩 박스의 좌표를 0으로 초기화
 
-                for green_box in self.green_boxes:
-                    x, y, w, h = green_box
-                    green_roi = frame[y:y+h, x:x+w]
-                    yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
-                    yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for green_box in self.green_boxes:
+                x, y, w, h = green_box
+                green_roi = frame[y:y+h, x:x+w]
+                yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
+                yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                    flag_centers = []
+                flag_centers = []
 
-                    for cnt in yellow_contours:
-                        area = cv2.contourArea(cnt)
-                        if area > 10:    
-                            rect = cv2.minAreaRect(cnt)
-                            box = cv2.boxPoints(rect)
-                            box = np.int0(box)
-                            max_x, min_x, max_y, min_y = self.getMaxMin(box)
-                            cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
-                            M = cv2.moments(cnt)
-                            if M['m00'] != 0:
-                                cx = int(M['m10'] / M['m00'])
-                                cy = int(M['m01'] / M['m00'])
-                                cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                                flag_centers.append((cx, cy))
+                for cnt in yellow_contours:
+                    area = cv2.contourArea(cnt)
+                    if area > 10:    
+                        rect = cv2.minAreaRect(cnt)
+                        box = cv2.boxPoints(rect)
+                        box = np.int0(box)
+                        max_x, min_x, max_y, min_y = self.getMaxMin(box)
+                        cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
+                        M = cv2.moments(cnt)
+                        if M['m00'] != 0:
+                            cx = int(M['m10'] / M['m00'])
+                            cy = int(M['m01'] / M['m00'])
+                            cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                            flag_centers.append((cx, cy))
 
-                    if flag_centers:
-                        farthest_flag_center = min(flag_centers, key=lambda center: center[1])
-                        cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
-                                    (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
-                        cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
-                        have_flag = True
+                if flag_centers:
+                    farthest_flag_center = min(flag_centers, key=lambda center: center[1])
+                    cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
+                                (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
+                    cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
+                    have_flag = True
                         
                 break
             #     cv2.imshow('프레임', frame)
