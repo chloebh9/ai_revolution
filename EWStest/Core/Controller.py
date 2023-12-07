56315a94147083cc3a20626dd56774185519d886
HyunWoo Cho <hwooo2255@gmail.com>
2023-12-07 09:39:22 +0900

    test

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -32,7 +32,7 @@ class Act(Enum):
 class Controller:
     robo: Robo = Robo()
     #act: Act = Act.START  # 순서도 시작
-    act: Act = Act.START
+    act: Act = Act.TEST
     # act: Act = Act.START가 시작 지점
 
     count_putting: int = 0  # 퍼팅 횟수
@@ -975,7 +975,13 @@ class Controller:
 
         ########################################################## # test
         if act == act.TEST:
-            self.robo._motion.turn("LEFT",45, 2)
+            self.robo._motion.turn("LEFT", 45)
+            self.robo._motion.turn("LEFT", 20, 2)
+
+            time.sleep(2)
+
+            self.robo._motion.turn("RIGHT", 45)
+            self.robo._motion.turn("RIGHT", 20, 2)
 
 
 
